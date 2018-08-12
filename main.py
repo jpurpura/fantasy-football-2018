97b7d889cc91ecff321e6efe333c5080658ba6fc
James Purpura <>
2018-08-12 14:09:21 -0700

    add comments within main()

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def main():
     roster_dict = util.read_json(league, c.ROSTER)
     score_keys, score_vals = util.separate_kv(scoring_dict)
     pos_dataframe_dict = {}
+
     for position, pos_df in prj.items():
-        pos_df = adj.adj_scoring(pos_df, score_keys, np.array(score_vals))
+        # Clean up player names
         names, teams, pos = adj.separate_names_teams_pos(pos_df)
         pos_df[c.PLAYER] = names
         pos_df.insert(2, c.TEAM, teams)
         pos_df.insert(3, c.POS, pos)
+
+        # Calculate league points and remove original points
+        new_pts = adj.adj_scoring(pos_df[score_keys], np.array(score_vals))
+        pos_df.insert(4, c.POINTS, new_pts)
+
         pos_df = adj.sort_by_pts(pos_df)
         pos_dataframe_dict[position] = pos_df
+
     for pos, pos_df in pos_dataframe_dict.items():
         print(pos)
         print(pos_df)
