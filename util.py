3b100354f81000ae5e2fe7f84627e10eb8747f92
James Purpura <>
2018-08-12 13:56:20 -0700

    file with utility functions

--- util.py
+++ util.py
@@ -10,3 +10,9 @@ def read_json(league: str, league_file: str) -> dict:
     with open("{}/{}.json".format(league, league_file)) as f:
         data = json.load(f)
     return data
+
+
+def separate_kv(d: dict) -> ([str], [str]):
+    keys = list(d)
+    values = [d[key] for key in keys]
+    return keys, values
