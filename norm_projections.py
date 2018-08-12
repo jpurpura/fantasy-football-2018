ca1c372fcb8b385490f6736308f7bcef04516e94
James Purpura <>
2018-08-12 13:56:05 -0700

    make plus and norm stats

--- norm_projections.py
+++ norm_projections.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
 
-def calc_plus(points: pd.Series, num_starters: int) -> pd.Series:
+def calc_plus(points: pd.Series, num_players: int) -> pd.Series:
     """ Series must be sorted in descending order by points"""
-    starters_mean = points.iloc[:num_starters].mean()
+    starters_mean = points.iloc[:num_players].mean()
     return points.divide(starters_mean)
 
 
 def calc_weekly(points: pd.Series, weeks=16) -> pd.Series:
     return points.divide(weeks)
 
+
+def calc_zscore_norm(points: pd.Series, num_players: int) -> pd.Series:
+    mean_diff = points - points.mean()
+    return mean_diff.divide(points.std())
